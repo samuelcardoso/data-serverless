86ba3cba16524b4f099df6207b6b75ff3373fd45
samuelcardosozup <samuel.cardoso@zup.com.br>
2020-10-08 21:16:53 -0300

    kinesis-consumer

--- processor/src/handler.ts
+++ processor/src/handler.ts
@@ -1,60 +1,105 @@
-import { KinesisStreamEvent, APIGatewayProxyEvent, Context } from 'aws-lambda';
+import {
+  KinesisStreamEvent,
+  APIGatewayProxyEvent,
+  Context,
+  APIGatewayProxyHandler,
+} from "aws-lambda";
+import { Kinesis }  from "aws-sdk";
+import { v4 as uuidv4 } from "uuid";
 
 export const corsHeaders = {
-  'X-Requested-With': '*',
-  'Access-Control-Allow-Headers': '*',
-  'Access-Control-Allow-Origin': '*',
-  'Access-Control-Allow-Methods': '*'
+  "X-Requested-With": "*",
+  "Access-Control-Allow-Headers": "*",
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Methods": "*",
 };
 
-export async function listener(event: Partial<KinesisStreamEvent>, context: Context): Promise<any> {
+export default class RequestUtils {
+  static getCors() {
+    return {
+      'X-Requested-With': '*',
+      'Access-Control-Allow-Headers': '*',
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': '*'
+    };
+  };
+}
+
+export async function listener(
+  event: Partial<KinesisStreamEvent>,
+  context: Context
+): Promise<any> {
   context.callbackWaitsForEmptyEventLoop = false;
   try {
     // console.log(JSON.stringify(event, null, 2));
     event.Records!.forEach((record) => {
       // Kinesis data is base64 encoded so decode here
-      const payload = Buffer.from(record.kinesis.data, 'base64').toString('ascii');
-      console.log('Decoded payload:', payload);
+      const payload = Buffer.from(record.kinesis.data, "base64").toString(
+        "ascii"
+      );
+      console.log("Decoded payload:", payload);
     });
-    console.log('teste');
-    return {
-      statusCode: 200,
-      body: JSON.stringify('AAAAAA'),
-      headers: {
-        ...corsHeaders
-      }
-    };
   } catch (err) {
     console.error(err);
   }
 }
 
-export async function send(event: Partial<APIGatewayProxyEvent>, context: Context): Promise<any> {
+export async function send(
+  event: Partial<APIGatewayProxyEvent>,
+  context: Context
+): Promise<any> {
   context.callbackWaitsForEmptyEventLoop = false;
+
+  const kinesis = new Kinesis({
+    apiVersion: "2013-12-02",
+  });
+
+  let statusCode: number = 200;
+  let message: string;
+
+  // if (!event.body) {
+  //   return {
+  //     statusCode: 400,
+  //     body: JSON.stringify({
+  //       message: "No body was found",
+  //     }),
+  //     headers: {
+  //       ...RequestUtils.getCors()
+  //     }
+  //   };
+  // }
+
+  const streamName: string = "myapp_stream";
+
   try {
-    const kinesis = new AWS.Kinesis({
-      endpoint: `${process.env.LAMBDA_KINESIS_HOST}:${process.env.LAMBDA_KINESIS_PORT}`,
-      region: process.env.LAMBDA_REGION,
-      apiVersion: '2002',
-      sslEnabled: false
-    });
-    // Read the records
-    const records = await BB.all(process.argv.slice(1).map(f => readAsync(f)));
-    // Write them to Kinesis
-    return BB.map(records, record => kinesis.putRecord({
-      Data: JSON.stringify(yaml.safeLoad(record)),
-      PartitionKey: '0',
-      StreamName: process.env.LAMBDA_KINESIS_STREAM_NAME
-    }).promise());
-    // console.log(JSON.stringify(event, null, 2));
-    return {
-      statusCode: 200,
-      body: JSON.stringify('AAAAAA'),
-      headers: {
-        ...corsHeaders
-      }
-    };
-  } catch (err) {
-    console.error(err);
+    await kinesis
+      .putRecord({
+        StreamName: streamName,
+        PartitionKey: uuidv4(),
+        Data: JSON.stringify({a: 3}),
+      })
+      .promise();
+
+    message = "Message placed in the Event Stream!";
+  } catch (error) {
+    console.log(error);
+    message = error;
+    statusCode = 500;
   }
+
+  return {
+    statusCode,
+    body: JSON.stringify({
+      message,
+    }),
+  };
+}
+
+export async function checkModel() {
+  
+
+}
+
+export async function trainModel() (
+  
 }
